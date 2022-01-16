537b4a911e5cff68f13ff024945d150a00fdc061
turbofish <ryokotmng@gmail.com>
2022-01-16 23:09:48 +0900

    create client struct and use context to cancel sending request

---
+++ client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type client struct {
+	endpoint string
+	*http.Client
+}
+
+func newClient(endpoint string) *client {
+	c := *http.DefaultClient
+	clone := &c
+	return &client{endpoint, clone}
+}
+
+func (c *client) sendRequest(ctx context.Context) {
+	req, err := http.NewRequest(*httpMethod, c.endpoint, nil)
+	if err != nil {
+		return
+	}
+	t := time.NewTicker(time.Duration(*interval) * time.Millisecond)
+	testStartTime := time.Now()
+	var numOfRequestsSent int
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			requestStartTime := time.Now()
+			res, err := c.Do(req)
+			if err != nil {
+				break
+			}
+			if err := res.Body.Close(); err != nil {
+				log.Fatalf("failed to close response body: %+v", err)
+			}
+			printDetails(numOfRequestsSent, time.Since(testStartTime).Milliseconds(), time.Since(requestStartTime).Milliseconds(), res)
+		}
+		numOfRequestsSent++
+	}
+	t.Stop()
+}
+
+func printDetails(numOfRequestsSent int, elapsedTime int64, latency int64, res *http.Response) {
+	fmt.Printf("number of requests sent: %v\n", numOfRequestsSent)
+	fmt.Printf("elapsed time of the test: %vms\n", elapsedTime)
+	fmt.Printf("latency: %vms\n", latency)
+	fmt.Printf("response(status code: %v) ", res.StatusCode)
+}
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"time"
 )
 
@@ -17,44 +15,9 @@ var (
 
 func main() {
 	flag.Parse()
-	cancel := make(chan struct{})
-	go sendRequest(cancel, *endpoint)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	c := newClient(*endpoint)
+	go c.sendRequest(ctx)
 	time.Sleep(time.Duration(*duration) * time.Second)
-	close(cancel)
-}
-
-func sendRequest(cancel chan struct{}, endpoint string) {
-	t := time.NewTicker(time.Duration(*interval) * time.Millisecond)
-	testStartTime := time.Now()
-	var numOfRequestsSent int
-	for {
-		select {
-		case <-cancel:
-			return
-		case <-t.C:
-			requestStartTime := time.Now()
-			switch *httpMethod {
-			case "PUT":
-				fmt.Println("unimplemented!")
-			default:
-				res, err := http.Get(endpoint)
-				if err != nil {
-					break
-				}
-				if err := res.Body.Close(); err != nil {
-					log.Fatalf("failed to close response body: %+v", err)
-				}
-				printDetails(numOfRequestsSent, time.Since(testStartTime).Milliseconds(), time.Since(requestStartTime).Milliseconds(), res)
-			}
-		}
-		numOfRequestsSent++
-	}
-	t.Stop()
-}
-
-func printDetails(numOfRequestsSent int, elapsedTime int64, latency int64, res *http.Response) {
-	fmt.Printf("number of requests sent: %v\n", numOfRequestsSent)
-	fmt.Printf("elapsed time of the test: %vms\n", elapsedTime)
-	fmt.Printf("latency: %vms\n", latency)
-	fmt.Printf("response(status code: %v) ", res.StatusCode)
 }
