4d5ba49992eec8fca018c9676e261947876af56d
turbofish <ryokotmng@gmail.com>
2022-01-16 21:40:21 +0900

    calculate latency and print the details per request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
@@ -22,20 +23,38 @@ func main() {
 	close(cancel)
 }
 
-func sendRequest(cancel chan struct{}, endPoint string) {
+func sendRequest(cancel chan struct{}, endpoint string) {
 	t := time.NewTicker(time.Duration(*interval) * time.Millisecond)
+	testStartTime := time.Now()
+	var numOfRequestsSent int
 	for {
 		select {
 		case <-cancel:
 			return
 		case <-t.C:
+			requestStartTime := time.Now()
 			switch *httpMethod {
 			case "PUT":
 				fmt.Println("unimplemented!")
 			default:
-				http.Get(endPoint)
+				res, err := http.Get(endpoint)
+				if err != nil {
+					break
+				}
+				if err := res.Body.Close(); err != nil {
+					log.Fatalf("failed to close response body: %+v", err)
+				}
+				printDetails(numOfRequestsSent, time.Since(testStartTime).Milliseconds(), time.Since(requestStartTime).Milliseconds(), res)
 			}
 		}
+		numOfRequestsSent++
 	}
 	t.Stop()
+}
+
+func printDetails(numOfRequestsSent int, elapsedTime int64, latency int64, res *http.Response) {
+	fmt.Printf("number of requests sent: %v\n", numOfRequestsSent)
+	fmt.Printf("elapsed time of the test: %vms\n", elapsedTime)
+	fmt.Printf("latency: %vms\n", latency)
+	fmt.Printf("response(status code: %v) ", res.StatusCode)
 }
--- receiver/main.go
+++ receiver/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", Printer)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func Printer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("request received -----")
+func handler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(10 * time.Millisecond)
+	fmt.Fprint(w, "Hello World from Go.")
 }
