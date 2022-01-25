2046e1b4298032ba460ceecc235285632533c35c
turbofish <ryokotmng@gmail.com>
2022-01-25 23:52:13 +0900

    fix receiver to return several kind of responses with different status codes

--- receiver/main.go
+++ receiver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -12,4 +13,15 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(10 * time.Millisecond)
+	rand.Seed(time.Now().UnixNano())
+	switch rand.Intn(100) % 5 {
+	case 1:
+		w.WriteHeader(http.StatusOK)
+	case 2:
+		w.WriteHeader(http.StatusFound)
+	case 3:
+		w.WriteHeader(http.StatusBadRequest)
+	case 4:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
